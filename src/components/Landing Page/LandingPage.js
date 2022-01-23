c0eb181f7d86a0983a32325ba0256ffe8cc23d18
Adib Ahmed <ahmedadib317@gmail.com>
2022-01-23 15:33:22 +0600

    hero section

--- src/components/Landing Page/LandingPage.js
+++ src/components/Landing Page/LandingPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import About from '../About/About';
 import Contact from '../Contact/Contact';
 import Intro from '../Intro/Intro';
-import Try from '../Intro/Try';
+// import Try from '../Intro/Try';
 import Navbar from '../Navbar/Navbar';
 import ProductList from '../ProductList/ProductList';
 
