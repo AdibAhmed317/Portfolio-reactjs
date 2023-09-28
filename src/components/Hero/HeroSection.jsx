94ddf392f13c692fa06ced8c9c6547e9ebebf74e
AdibAhmed317 <ahmedadib317@gmail.com>
2023-09-28 21:03:13 +0600

    resume update

--- src/components/Hero/HeroSection.jsx
+++ src/components/Hero/HeroSection.jsx
@@ -54,7 +54,7 @@ const HeroSection = () => {
           <section className='mt-5 lg:mt-8 w-32 text-base py-3 px-6 text-white rounded bg-transparent border-yellow-200 border-2 hover:bg-yellow-200 hover:text-black font-mono text-center'>
             <a
               target='_blank'
-              href='https://drive.google.com/file/d/1ZPsPo9iOZb4qGsr_ePqGeFmA6JiubUm6/view?usp=drive_link'>
+              href='https://drive.google.com/file/d/1MIpg025gqU0d5-w4diuQdZD0p9OFY9r1/view?usp=sharing'>
               Resume
             </a>
           </section>
